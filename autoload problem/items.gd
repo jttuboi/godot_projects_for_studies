529057d3120ef2dc68dcc2f6fa63c6ea11d69f4d
jttuboi <tuboi.jairo@gmail.com>
2021-06-27 09:29:19 -0300

    test for understand the problem of using const of autoload script in another autoload script

---
+++ autoload problem/items.gd
@@ -0,0 +1,23 @@
+extends Node
+
+
+const TYPE = {
+	WEAPON = 1000,
+	FOOD = 2000,
+	RESOURCE = 3000
+}
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ autoload problem/storages.gd
@@ -0,0 +1,24 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+const bag = {
+	slots = [
+		{ items_accepted = [ Items.TYPE.FOOD ] },
+		{ items_accepted = [ Items.TYPE.WEAPON ] }
+	]
+}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
