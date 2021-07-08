6bb380b2f6f5f0f6172bd56d71b5b016801d4d8a
jttuboi <tuboi.jairo@gmail.com>
2021-07-08 00:45:00 -0300

    add menu and container test

--- playground/World.gd
+++ playground/World.gd
@@ -1,2 +1,15 @@
-extends Node2D
+extends Node
 
+
+func _ready():
+	pass
+
+
+func _on_ExplosionEffect_pressed():
+	clear_container()
+
+
+func clear_container():
+	for node in $Container.get_children():
+		$Container.remove_child(node)
+		node.queue_free()
